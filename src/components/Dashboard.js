4d860f24cd117bea9a6a06c314109acb7621d6b5
Dayoung <dy.kim@datadriven.kr>
2021-06-30 00:12:59 +0900

    todolist 기본기능 붙이기

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router";
 import { useAuth } from "contexts/Auth";
 
@@ -12,10 +13,68 @@ export function Dashboard() {
     history.push("/login");
   }
 
+  //todolist 기능
+  const [userInput, setUserInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUserInput(e.target.value);
+    console.log(userInput);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodoList([userInput, ...todoList]);
+    setUserInput("");
+  };
+
+  const handleDelete = (todo) => {
+    const updatedArr = todoList.filter(
+      (todoItem) => todoList.indexOf(todoItem) != todoList.indexOf(todo)
+    );
+    setTodoList(updatedArr);
+  };
+
   return (
-    <div>
-      <p>Welcome, {user?.id}!</p>
-      <button onClick={handleSignOut}>Sign out</button>
-    </div>
+    <>
+      <div>
+        <p>Welcome, {user?.id}!</p>
+        <button onClick={handleSignOut}>Sign out</button>
+      </div>
+      <div>
+        <form>
+          <input
+            className="col-6"
+            value={userInput}
+            type="text"
+            onChange={handleChange}
+          />
+          <button type="button" onClick={handleSubmit}>
+            확인
+          </button>
+        </form>
+        <ul>
+          {todoList.length >= 1
+            ? todoList.map((todo, idx) => {
+                return (
+                  <li key={idx}>
+                    {todo}
+                    <button
+                      type="button"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleDelete(todo);
+                      }}
+                    >
+                      삭제
+                    </button>
+                  </li>
+                );
+              })
+            : "항목을 입력하시오!"}
+        </ul>
+      </div>
+    </>
   );
 }
